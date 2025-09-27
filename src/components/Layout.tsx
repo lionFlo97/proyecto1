3403bb9914b65b353c2288ada0580217e343837c
lionFlo97 <leonflwers@gmail.com>
2025-09-26 23:58:04 -0500

    Updated Layout.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,1 +1,19 @@
 import Layout from "./Layout";
+import React from "react";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+const Layout: React.FC<Props> = ({ children }) => {
+  return (
+    <div className="layout">
+      <header className="header">
+        <h1>Control de Bodega</h1>
+      </header>
+      <main>{children}</main>
+    </div>
+  );
+};
+
+export default Layout;
